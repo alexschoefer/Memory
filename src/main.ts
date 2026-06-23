92cb02aad5e9106aeb2019cb0fe20eced5449a51
alexschoefer <alex.schoefer@gmx.net>
2026-06-23 22:21:09 +0200

    refactor: delete empty lines + structure of functions

--- src/main.ts
+++ src/main.ts
@@ -28,20 +28,14 @@ document.getElementById("exit-game-btn")?.addEventListener("click", () => {
 
 });
 
-
 // Game-over and return to settings
 document.getElementById("go_back_btn")?.addEventListener("click", () => {
     showCurrentPage("settings-page");
     setCurrentTheme("settings-theme");
     resetGame();
-    document
-        .querySelectorAll(".page")
-        .forEach(page => page.classList.remove("page--active"));
-
+    document.querySelectorAll(".page").forEach(page => page.classList.remove("page--active"));
     const page = document.getElementById("settings-page");
-
     if (!page) return;
-
     page.classList.add("page--active");
 });
 
--- src/ts/pages/game-over.ts
+++ src/ts/pages/game-over.ts
@@ -23,47 +23,41 @@ export function getWinner(scores: Scores): "blue" | "orange" | "draw" {
 
 export function showGameOverScreen(winner: "blue" | "orange" | "draw"): void {
 
-    document
-        .querySelectorAll(".page")
-        .forEach(page => page.classList.remove("page--active"));
-
+    document.querySelectorAll(".page").forEach(page => page.classList.remove("page--active"));
     const page = document.getElementById("game_over");
 
     if (!page) return;
 
     page.classList.add("page--active");
 
-    const winnerText = page.querySelector(
-        ".game-over__winner"
-    ) as HTMLElement;
+    const winnerText = page.querySelector(".game-over__winner") as HTMLElement;
 
-    const winnerPlayer = page.querySelector(
-        ".game-over__endscreen p span"
-    ) as HTMLElement;
+    const winnerPlayer = page.querySelector(".game-over__endscreen p span") as HTMLElement;
 
-    const winnerImage = page.querySelector(
-        ".game-over__winner_img"
-    ) as HTMLElement;
+    const winnerImage = page.querySelector(".game-over__winner_img") as HTMLElement;
 
     if (!winnerText || !winnerPlayer || !winnerImage) return;
 
     if (winner === "blue") {
         winnerText.textContent = "The winner is";
-        winnerPlayer.textContent = "Blue";
+        winnerPlayer.textContent = "Blue Player";
+        winnerPlayer.className = "game-over__winner_blue";
         winnerImage.className =
             "game-over__winner_img winner-blue";
     }
 
     else if (winner === "orange") {
         winnerText.textContent = "The winner is";
-        winnerPlayer.textContent = "Orange";
+        winnerPlayer.textContent = "Orange Player";
+        winnerPlayer.className = "game-over__winner_orange";
         winnerImage.className =
             "game-over__winner_img winner-orange";
     }
 
     else {
-        winnerText.textContent = "It's a draw!";
-        winnerPlayer.textContent = "";
+        winnerText.textContent = "It's a";
+        winnerPlayer.textContent = "DRAW";
+        winnerPlayer.className = "game-over__winner_draw";
         winnerImage.className =
             "game-over__winner_img winner-draw";
     }
